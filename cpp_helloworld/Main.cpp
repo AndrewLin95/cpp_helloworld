2a373136dcca145216e637974483dd02b3fa7873
AndrewLin95 <andrew.lin88@hotmail.com>
2023-03-25 20:24:18 -0400

    class

--- cpp_helloworld/Main.cpp
+++ cpp_helloworld/Main.cpp
@@ -1,22 +1,54 @@
 #include <iostream>
-#include "Log.h"
-
-void Log(int number)
-{
-	std::cout << number << std::endl;
-}
 
 void Increment(int* value)
 {
 	(*value)++;
 }
 
+class Log
+{
+public:
+	const int LogLevelError = 0;
+	const int LogLevelWarning = 1;
+	const int LogLevelInfo = 2;
+private:
+	int m_LogLevel = LogLevelInfo;
+public:
+	void SetLevel(int level)
+	{
+		m_LogLevel = level;
+	}
+
+	void Error(const char* message)
+	{
+		if (m_LogLevel >= LogLevelError)
+			std::cout << "[ERROR]:" << message << std::endl;
+	}
+
+	void Warn(const char* message)
+	{
+		if (m_LogLevel >= LogLevelWarning)
+			std::cout << "[WARNING]:" << message << std::endl;
+	}
+
+	void Info(const char* message)
+	{
+		if (m_LogLevel >= LogLevelInfo)
+			std::cout << "[INFO]:" << message << std::endl;
+	}
+};
+
 int main()
 {
-	int var = 8;
-	Increment(&var);
+	//int var = 8;
+	//Increment(&var);
 
-	Log(var);
+	//int& ref = var;
+
+	Log log;
+	log.SetLevel(log.LogLevelWarning);
+	log.Warn("hello!");
+
 
 	std::cin.get();
 }
